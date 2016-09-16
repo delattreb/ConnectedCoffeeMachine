d0196a8a59c4b56eeb51ef72071a2137a782496b
Bruno <delattreb@gmail.com>
2016-09-16 11:19:04 +0200

    Suppression des lib inutiles

--- src/main.py
+++ src/main.py
@@ -4,15 +4,15 @@ Auteur: Bruno DELATTRE
 Date : 12/08/2016
 """
 
-from lib import com_email, com_gpio
-
-# from lib import com_config
+from lib import com_ca
 
 # com_config.setConfig()
 
 #com_email.send_mail_gmail('tes', 'texte')
 #if com_email.getIFFT() != '':
 #    pass
+
+
 
 gpio = com_gpio.GPIO('GPIO', 'log_gpio.txt')
 gpio.setmodeBOARD()
